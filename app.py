014e2347466bc6d5e8e195707e692d6695ccac54
Leonardo Cazarine <leocazarine@gmail.com>
2022-03-30 15:18:49 -0300

    Primeira tentativa de implementação dos consents

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-import re
+from datetime import datetime, timedelta
+
 from flask import Flask, request, redirect, jsonify
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
 
+import requests
+from requests.structures import CaseInsensitiveDict
+
 app = Flask(__name__)
-# cors = CORS(app, resources={r"/compartilhar": {"origins": "*"}})
-cors = CORS(app)
+cors = CORS(app, resources={r"/compartilhar": {"origins": "*"}})
+# cors = CORS(app)
 api = Api(app)
 
 #######################################################################
@@ -15,24 +19,46 @@ api = Api(app)
 @app.route("/consent", methods=['POST'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def criar_consentimento():
-    print("-- criar_consentimento --")
     request_form = request.form
 
-    print("####")
-    print(request_form.to_dict())
-
-    nome = request_form.get("nome")
-    cpf = request_form.get("cpf")
-    objetivoCompatilhamento = request_form.get("objetivoCompatilhamento")
-    instituicao = request_form.get("instituicao")
-    dadosObrigatorios = request_form.get("dadosObrigatorios")
-    prazo = request_form.get("prazo")
+    nome = request_form["nome"]
+    cpf = request_form["cpf"]
 
     # TODO Call mock bank
 
-    response = jsonify({'resposta': request.form})
-    return response, 200
+    headers = {
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
+    }
 
+    json_data = {
+        'data': {
+            'loggedUser': {
+                'document': {
+                    'identification': cpf,
+                    'rel': 'CPF',
+                },
+            },
+            'permissions': [
+                'ACCOUNTS_READ',
+                'ACCOUNTS_OVERDRAFT_LIMITS_READ',
+                'RESOURCES_READ',
+            ],
+            'expirationDateTime': (datetime.now()+timedelta(days=30)).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'only_one_client_in_database': 'True',
+        },
+    }
+
+    response = requests.post('https://mango-mockbank.herokuapp.com/auth/consents/consents/', headers=headers, json=json_data)
+    
+    response = jsonify({
+        'resposta1': request.form,
+        'resposta2': response.json()
+    })
+
+    redirect("https://mango-mockbank.herokuapp.com/auth/auth/?response_type=token&only_one_client_in_database=True/  ")
+
+    return response
 
 @app.route("/consents", methods=['GET'])
 # @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
@@ -61,23 +87,8 @@ def get_conta_indo():
 @app.route("/post_new_consent", methods=['POST'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def postar_novo_consentimento():
-    form = request.form
-    nome = form.get("name")
-    cpf = form.get("cpf")
-
-    print("####")
-    print(nome)
-    print(cpf)
-
-    # response = request.post(url='http://localhost:3000/compartilhamento')
-
-    # return form, 200
-
-    return redirect('http://localhost:3000/compartilhamento')
-
-    # print("####")
-
-    # return form, 200
+    # Redirecionamento para o Front passando CPF e nome
+    return "Front", 200
 
 
 if __name__ == "__main__":
