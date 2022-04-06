9d35c92279cbbe73c70ecb1c0f63c563ad90dafe
Miguel Ferreira <miguel.ferreira@letscode.com.br>
2022-04-05 23:23:30 -0300

    Implemeting some endpoint to integrate

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from datetime import datetime, timedelta
+from pickle import FALSE, TRUE
 
 from flask import Flask, request, redirect, jsonify
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
 
-import json
 import requests
-import urllib
-from requests.structures import CaseInsensitiveDict
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/compartilhar": {"origins": "*"}})
@@ -29,17 +27,14 @@ def criar_consentimento():
 
     nome = request_form["nome"]
     cpf = request_form["cpf"]
+    instituicao = request_form["instituicao"]
     print(nome)
     print(cpf)
-    instituicao = request_form["instituicao"]
     print(instituicao)
-    
-    return jsonify({"redirect_url": "https://mango-mockbank.herokuapp.com/auth/auth/?response_type=token&only_one_client_in_database=True/"}), 200, {'ContentType':'application/json'}
-    # TODO Call mock bank
 
     headers = {
         # Already added when you pass json= but not when you pass data=
-        # 'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     }
 
     json_data = {
@@ -57,19 +52,22 @@ def criar_consentimento():
             ],
             'expirationDateTime': (datetime.now()+timedelta(days=30)).strftime('%Y-%m-%dT%H:%M:%SZ'),
             'only_one_client_in_database': 'True',
-        },
+        }
     }
 
     response = requests.post('https://mango-mockbank.herokuapp.com/auth/consents/consents/', headers=headers, json=json_data)
     
-    response = jsonify({
-        'resposta1': request.form,
-        'resposta2': response.json()
-    })
 
-    redirect("https://mango-mockbank.herokuapp.com/auth/auth/?response_type=token&only_one_client_in_database=True/")
+    return jsonify({"redirect_url": "https://mango-mockbank.herokuapp.com/auth/auth/?response_type=token&only_one_client_in_database=True/"}), 200, {'ContentType':'application/json'}
+    
 
-    return response
+@app.route("/teste", methods=['POST'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def teste():
+    request_form = request.get_json()
+    print(request_form)
+    return "sucesso", 200
+
 
 @app.route("/consents", methods=['GET'])
 # @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
@@ -89,17 +87,54 @@ def deletar_consentimento(consentID):
 
 
 @app.route("/get_conta", methods=['GET'])
-# @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-def get_conta_indo():
-    algo = request.form
-    return algo, 200
-
-
-@app.route("/post_new_consent", methods=['POST'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-def postar_novo_consentimento():
-    # Redirecionamento para o Front passando CPF e nome
-    return "Front", 200
+def get_conta():
+    # Postman
+    request_form = request.form
+    # Frontend
+    # request_form = request.get_json()
+    token = request_form["token"]
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get('https://mango-mockbank.herokuapp.com/accounts/balances', headers=headers)
+
+    return response.json(), 200
+
+
+@app.route("/validate_consent", methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def validate_consent():
+    # Postman
+    request_form = request.form
+    # Frontend
+    # request_form = request.get_json()
+    cpf = request_form["cpf"]
+
+    # Mockbank
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'data': {
+            'user_cpf': cpf,
+            'only_one_client_in_database': 'True',
+        },
+    }
+
+    response = requests.get('https://mango-mockbank.herokuapp.com/auth/consents/consents/', headers=headers, json=json_data)
+
+    response_dict = response.json()
+    status = response_dict['data']['status']
+    expiration_date_time = datetime.strptime(response_dict['data']['expirationDateTime'], '%Y-%m-%dT%H:%M:%SZ')
+
+    if status == "AUTHORISED" and expiration_date_time > datetime.now():
+        return "True", 200
+    else:
+        return "False", 200
 
 
 if __name__ == "__main__":
